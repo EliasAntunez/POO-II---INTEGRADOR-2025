4db930141fc9df58d93a2ac2507e6d474d5580b1
ile <melidonda524@gmail.com>
2025-11-29 02:20:50 -0300

    Dejé un par de comentarios ESCRITOS EN MAYÚSCULAS

--- facturacion/facturacion/src/main/java/com/example/facturacion/modelo/Pago.java
+++ facturacion/facturacion/src/main/java/com/example/facturacion/modelo/Pago.java
@@ -6,10 +6,31 @@ import java.time.LocalDateTime;
 
 import com.example.facturacion.modelo.enums.MedioPago;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * SI BIEN HAY ALGO IMPLEMENTADO, HAY QUE CAMBIAR TODO.
+ * A RAÍZ DE LOS CAMBIOS HECHOS EL MÓDULO DE FACTURACIÓN, TODO ESTE MÓDULO SE DEBE DE HACER DE CERO
+*/
 
 /**
  * Entidad Pago.
--- facturacion/facturacion/src/main/java/com/example/facturacion/modelo/enums/MedioPago.java
+++ facturacion/facturacion/src/main/java/com/example/facturacion/modelo/enums/MedioPago.java
@@ -1,6 +1,11 @@
 package com.example.facturacion.modelo.enums;
 
 /**
+ * ESTE ENUM ES PARA EL MÓDULO DE PAGOS. SE IMPLEMENTÓ, PERO DE SEGURO ESTÁ MAL.
+ * HAY QUE REVISARLO CUANDO SE HAGA EL MÓDULO DE PAGOS.
+ */
+
+/**
  * Medios de pago disponibles.
  */
 public enum MedioPago {
